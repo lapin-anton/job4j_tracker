f4d584c3fa53001e987da08764b76d68ed4b410a
anton-lapin <lapin_antoine@mail.ru>
2022-03-27 10:04:12 +0300

    1.1. Создание объекта [#165452 #280559]

--- src/main/java/ru/job4j/oop/Dog.java
+++ src/main/java/ru/job4j/oop/Dog.java
@@ -3,7 +3,9 @@ package ru.job4j.oop;
 public class Dog {
 
     public static void main(String[] args) {
-        Dog goofy = new Dog();
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhuchka = new Dog();
     }
 
 }
