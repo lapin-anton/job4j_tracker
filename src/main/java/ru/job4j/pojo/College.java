fbaeee5c3c99c25ba29f20bb285633b09bc6c319
anton-lapin <lapin_antoine@mail.ru>
2022-03-29 15:34:28 +0300

    2. Модель данных. [#174683 #281535]

--- src/main/java/ru/job4j/pojo/College.java
+++ src/main/java/ru/job4j/pojo/College.java
@@ -8,7 +8,7 @@ public class College {
         student.setFio("Studentov Student Studentovich");
         student.setGroup("1-37");
         student.setAdmission(LocalDate.parse("2020-09-01"));
-        System.out.printf("Student info: \n\tfio: %s\n\tgroup: %s\n\tadmission date: %s%n",
+        System.out.printf("Student info: %n\tfio: %s%n\tgroup: %s%n\tadmission date: %s%n",
                 student.getFio(), student.getGroup(), student.getAdmission());
     }
 }
