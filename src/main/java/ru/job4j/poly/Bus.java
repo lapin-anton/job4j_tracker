ea5c25139f39c40c1e454cb1fe9d4a067cfdf87c
anton-lapin <lapin_antoine@mail.ru>
2022-04-01 21:49:14 +0300

    3. Интерфейс Input [#181092 #282842]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -3,16 +3,17 @@ package ru.job4j.poly;
 public class Bus implements Transport {
     @Override
     public void move() {
-
+        System.out.println("The bus is moving...");
     }
 
     @Override
     public void passengers(int amount) {
-
+        System.out.printf("%s passengers got on the bus.%n", amount);
     }
 
     @Override
     public double refuel(int fuel) {
-        return 0;
+        double pricePerLiter = 50.00;
+        return pricePerLiter * fuel;
     }
 }
