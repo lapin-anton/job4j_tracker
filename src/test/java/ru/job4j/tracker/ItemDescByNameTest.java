c8d9d765946aeae3574bb594e25a7116782b1580
anton-lapin <lapin_antoine@mail.ru>
2022-04-10 22:02:45 +0300

    0. Сортировка [#10096]

---
+++ src/test/java/ru/job4j/tracker/ItemDescByNameTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class ItemDescByNameTest {
+    @Test
+    public void sort() {
+        List<Item> items = new ArrayList<>(Arrays.asList(
+                new Item(1, "bug"),
+                new Item(100, "release"),
+                new Item(58, "analyst"),
+                new Item(6, "development")
+        ));
+        List<Item> expected = new ArrayList<>(Arrays.asList(
+                items.get(1),
+                items.get(3),
+                items.get(0),
+                items.get(2)
+        ));
+        Collections.sort(items, new ItemDescByName());
+        assertThat(items, is(expected));
+    }
+}
