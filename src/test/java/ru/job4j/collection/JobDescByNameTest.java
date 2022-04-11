79d6f1276f3b989a9268341c577ba1d312292d99
anton-lapin <lapin_antoine@mail.ru>
2022-04-11 20:37:13 +0300

    2. Комбинированный компаратор [#10036]

---
+++ src/test/java/ru/job4j/collection/JobDescByNameTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class JobDescByNameTest {
+    @Test
+    public void sort() {
+        List<Job> jobs = Arrays.asList(
+                new Job("Release", 1),
+                new Job("Fix bug", 4),
+                new Job("Analyst", 2),
+                new Job("X task", 0)
+        );
+        List<Job> expected = Arrays.asList(
+                jobs.get(3),
+                jobs.get(0),
+                jobs.get(1),
+                jobs.get(2)
+        );
+        Collections.sort(jobs, new JobDescByName());
+        assertThat(jobs, is(expected));
+    }
+}
