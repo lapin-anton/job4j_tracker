37d34faf37264e202aaab5dafba2680a1f000c6f
anton-lapin <lapin_antoine@mail.ru>
2022-04-17 18:44:58 +0300

    1

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 public class Profiles {
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
-                .map(Profile::getAddress)
+                .map(p -> p.getAddress())
                 .collect(Collectors.toList());
     }
 
