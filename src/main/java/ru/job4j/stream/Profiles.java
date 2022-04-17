1e645345c02adc3af661b3ba869c6396bc4623e4
anton-lapin <lapin_antoine@mail.ru>
2022-04-17 18:45:29 +0300

    2. Список адресов [#110062 #289938]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 public class Profiles {
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
-                .map(p -> p.getAddress())
+                .map(Profile::getAddress)
                 .collect(Collectors.toList());
     }
 
