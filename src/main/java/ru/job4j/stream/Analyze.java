4c9ba50873f077531548f5a142134617db046e5d
anton-lapin <lapin_antoine@mail.ru>
2022-04-20 22:14:39 +0300

    2. Аттестация [#49777]

---
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -0,0 +1,52 @@
+package ru.job4j.stream;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Analyze {
+    public static double averageScore(Stream<Pupil> stream) {
+        return stream.flatMap(p -> p.getSubjects().stream())
+                .flatMapToInt(s -> IntStream.of(s.getScore()))
+                .average()
+                .orElse(0L);
+    }
+
+    public static List<Tuple> averageScoreBySubject(Stream<Pupil> stream) {
+        return stream.map(p -> new Tuple(p.getName(), p.getSubjects().stream()
+                .mapToInt(Subject::getScore)
+                .average()
+                .orElse(0D)))
+                .collect(Collectors.toList());
+    }
+
+    public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
+        return stream.flatMap(p -> p.getSubjects().stream())
+                .collect(Collectors.groupingBy(Subject::getName,
+                        LinkedHashMap::new,
+                        Collectors.averagingDouble(Subject::getScore))
+                ).entrySet().stream()
+                .map(e -> new Tuple(e.getKey(), e.getValue()))
+                .collect(Collectors.toList());
+    }
+
+    public static Tuple bestStudent(Stream<Pupil> stream) {
+        return stream.map(p -> new Tuple(p.getName(), p.getSubjects().stream()
+                        .mapToInt(Subject::getScore)
+                        .sum()))
+                .max(Comparator.comparingDouble(Tuple::getScore))
+                .orElse(null);
+    }
+
+    public static Tuple bestSubject(Stream<Pupil> stream) {
+        return stream.flatMap(p -> p.getSubjects().stream())
+                .collect(Collectors.groupingBy(Subject::getName,
+                        LinkedHashMap::new,
+                        Collectors.summingDouble(Subject::getScore))
+                ).entrySet().stream()
+                .map(e -> new Tuple(e.getKey(), e.getValue()))
+                .max(Comparator.comparingDouble(Tuple::getScore))
+                .orElse(null);
+    }
+}
